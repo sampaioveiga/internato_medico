8183bc6293a2ca4dab20d7e73c3eefc49ed2d862
Miguel Sampaio <sampaio.veiga@gmail.com>
2013-09-18 15:11:59 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InternatoMedico::Application.config.secret_key_base = 'cdb8394d7761502e0096a6e9ed9075afc14e826663d0638a951f00702f3e9f90e66dfb42fb3e447ce3bb48d7f1a84ba043606055125da1ca0bbe184f71f726a6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+InternatoMedico::Application.config.session_store :cookie_store, key: '_internato_medico_session'
