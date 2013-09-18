75a90af55cc3a1b71dd2b426edbd1aeaae6f333b
Miguel Sampaio <sampaio.veiga@gmail.com>
2013-09-18 15:17:22 +0100

    Prepared App

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-InternatoMedico::Application.config.secret_key_base = 'cdb8394d7761502e0096a6e9ed9075afc14e826663d0638a951f00702f3e9f90e66dfb42fb3e447ce3bb48d7f1a84ba043606055125da1ca0bbe184f71f726a6'
+#InternatoMedico::Application.config.secret_key_base = 'cdb8394d7761502e0096a6e9ed9075afc14e826663d0638a951f00702f3e9f90e66dfb42fb3e447ce3bb48d7f1a84ba043606055125da1ca0bbe184f71f726a6'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+InternatoMedico::Application.config.secret_key_base = secure_token
